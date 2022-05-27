071457ff287f977b2245c30109c555f6a207d1fc
Anna Lopatina <an.lopatina050@gmail.com>
2022-05-27 18:52:02 +0300

    Initial commit

---
+++ MessagingUsingQueue/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+
+namespace MessagingUsingQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Service Bus connection string:");
+            string serviceBusConnectionString = Console.ReadLine();
+
+            Console.WriteLine("Enter queue name:");
+            string queueName = Console.ReadLine();
+
+            Console.WriteLine("Sending a message to a queue...");
+            SendMessageAsync(serviceBusConnectionString, queueName).GetAwaiter().GetResult();
+
+            Console.WriteLine("Receiving a message from a queue...");
+            ReceiveMessageAsync(serviceBusConnectionString, queueName).GetAwaiter().GetResult();
+        }
+
+        static async Task SendMessageAsync(string serviceBusConnectionString, string queueName)
+        {
+            await using var client = new ServiceBusClient(serviceBusConnectionString);
+            await using ServiceBusSender sender = client.CreateSender(queueName);
+
+            try
+            {
+                var message = new ServiceBusMessage($"Testing {DateTime.Now}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+
+        static async Task ReceiveMessageAsync(string serviceBusConnectionString, string queueName)
+        {
+            var client = new ServiceBusClient(serviceBusConnectionString);
+
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+
+            await using ServiceBusProcessor processor = client.CreateProcessor(queueName, processorOptions);
+
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            Console.Read();
+
+            await processor.CloseAsync();
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MessagingUsingTopic/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace MessagingUsingTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Service Bus connection string:");
+            string serviceBusConnectionString = Console.ReadLine();
+
+            Console.WriteLine("Enter topic name:");
+            string topicName = Console.ReadLine();
+
+            Console.WriteLine("Enter subscription name:");
+            string subscriptionName = Console.ReadLine();
+
+            Console.WriteLine("Sending a message to a topic...");
+            SendMessageAsync(serviceBusConnectionString, topicName).GetAwaiter().GetResult();
+
+            Console.WriteLine("Retrieving a message from topic...");
+            ReceiveMessageAsync(serviceBusConnectionString, topicName, subscriptionName).GetAwaiter().GetResult();
+        }
+
+        static async Task SendMessageAsync(string serviceBusConnectionString, string topicName)
+        {
+            await using var client = new ServiceBusClient(serviceBusConnectionString);
+            await using ServiceBusSender sender = client.CreateSender(topicName);
+
+            try
+            {
+                var message = new ServiceBusMessage($"Testing {DateTime.Now}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+        static async Task ReceiveMessageAsync(string serviceBusConnectionString, string topicName, string subscriptionName)
+        {
+            var client = new ServiceBusClient(serviceBusConnectionString);
+
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+
+            ServiceBusProcessor processor = client.CreateProcessor(topicName, subscriptionName, processorOptions);
+
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            Console.Read();
+
+            await processor.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{args.Message.SequenceNumber} Body:{args.Message.Body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {args.FullyQualifiedNamespace}");
+            Console.WriteLine($"- Entity Path: {args.EntityPath}");
+            Console.WriteLine($"- Executing Action: {args.ErrorSource}");
+            return Task.CompletedTask;
+        }
+    }
+}
